fdda0cfb54936da397e47cc329221eb5a58a0a85
Vlad Marginean <vladmarginean@softwiz.ro>
2024-12-18 21:09:34 +0200

    fix dotenv

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
-import dotenv from "dotenv";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "starfx/react";
 import App from "./App"; // Assuming App is your main component
 import { schema } from "./state/schema";
 import { setupState } from "./state/store";
-dotenv.config();
 const { store } = await setupState();
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
